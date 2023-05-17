5c9a73245dd12fa8cb7b50f01bc548f4ffcfe115
Betsy Tsoi <tsoisybetsy@gmail.com>
2023-05-17 22:15:16 +0900

    Implement redirect on NotFound page

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,8 +1,18 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
 export default function NotFound() {
+
+  const navigate = useNavigate();
+  useEffect(() => {
+    setTimeout(() => {
+      navigate('/', {replace: true});
+    }, 2000);
+  }, []);
 
   return (
     <div className="NotFound">
-      Page Not Found
+      Page Not Found. Redirecting to homepage...
     </div>
   );
 }
